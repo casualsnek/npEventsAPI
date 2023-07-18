8abb0912880072c23df211d2b4f2d5ac197a05c0
Casu Al Snek <casualsnek@pm.me>
2023-07-18 16:20:20 +0545

    Add more artefacts

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+import markdown
 import json
 import os
 from sqlalchemy import or_, and_
 from datetime import datetime
-
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -51,11 +51,11 @@ class CalendarQueryException(Exception):
 
 def get_days_on(caltype: str, year: int, month: int):
     dbc = db.select(Calendar).filter(
-            getattr(Calendar, f'{caltype}_year') == year,
-            getattr(Calendar, f'{caltype}_month') == month
-            ).order_by(
-                getattr(Calendar, f'{caltype}_day').desc()
-            )
+        getattr(Calendar, f'{caltype}_year') == year,
+        getattr(Calendar, f'{caltype}_month') == month
+    ).order_by(
+        getattr(Calendar, f'{caltype}_day').desc()
+    )
     result = db.session.execute(dbc).first()
     return getattr(result[0], f'{caltype}_day')
 
@@ -71,7 +71,7 @@ def get_today(caltype: str):
             Calendar.ad_year == y,
             Calendar.ad_month == m,
             Calendar.ad_day == d
-            )
+        )
         r = db.session.execute(dbc).first()
         return f'{r[0].bs_year}-{r[0].bs_month}-{r[0].bs_day}'
 
@@ -79,19 +79,19 @@ def get_today(caltype: str):
 def calendar_var_replace(caltype: str, string: str) -> str:
     string = string.replace(
         '@today', get_today(caltype=caltype)
-        )
+    )
     string = string.replace(
         '@ignore', '0'
-        )
+    )
     string = string.replace(
         '@cur_year', get_today(caltype=caltype).split('-')[0]
-        )
+    )
     string = string.replace(
         '@cur_month', get_today(caltype=caltype).split('-')[1]
-        )
+    )
     string = string.replace(
         '@cur_day', get_today(caltype=caltype).split('-')[2]
-        )
+    )
     return string
 
 
@@ -103,12 +103,12 @@ def calender_query_builder(caltype: str, start: tuple[int, int, int],
                            search_event: str = ''):
     filter_tithis = [
         item for item in filter_tithis if item.strip() != ''
-        ]
-    calendar = db.select(Calendar)\
+    ]
+    calendar = db.select(Calendar) \
         .join(
-            CalendarEventRelationship,
-            Calendar.id == CalendarEventRelationship.day_id
-            )\
+        CalendarEventRelationship,
+        Calendar.id == CalendarEventRelationship.day_id
+    ) \
         .group_by(Calendar.id) \
         .join(Events, CalendarEventRelationship.event_id == Events.id)
     if caltype not in ['ad', 'bs']:
@@ -116,7 +116,7 @@ def calender_query_builder(caltype: str, start: tuple[int, int, int],
     if end[0] > start[0] and end[0] == 0:
         raise CalendarQueryException(
             'End date is ahead of start date, cannot compute'
-            )
+        )
     if end[0] != 0:
         start_y, start_m, start_d = start[0], start[1], start[2]
         end_y, end_m, end_d = end[0], end[1], end[2]
@@ -129,7 +129,7 @@ def calender_query_builder(caltype: str, start: tuple[int, int, int],
                 f' "{start_m}" or "{end_m}" is not a valid month parameter. '
                 f'For range start use "0" to select first month of the year '
                 f'and for range end use "0" to select last month of the year !'
-                )
+            )
         start_d_data = get_days_on(caltype, start_y, start_m)
         end_d_data = get_days_on(caltype, end_y, end_m)
         # Validate day
@@ -152,17 +152,17 @@ def calender_query_builder(caltype: str, start: tuple[int, int, int],
                 int(f'{start_y}{start_m:02d}{start_d:02d}'),
                 int(f'{end_y}{end_m:02d}{end_d:02d}'
                     )
-                )
             )
+        )
     else:
         if start[0] != 0:
             calendar = calendar.where(
                 getattr(Calendar, f'{caltype}_year') == start[0]
-                )
+            )
         if start[1] > 0 and start[1] <= 12:
             calendar = calendar.where(
                 getattr(Calendar, f'{caltype}_month') == start[1]
-                )
+            )
         else:
             if start[1] != 0:
                 raise CalendarQueryException(
@@ -173,18 +173,18 @@ def calender_query_builder(caltype: str, start: tuple[int, int, int],
         if start[1] != 0:
             day_in_month = get_days_on(
                 caltype=caltype, year=start[0], month=start[1]
-                )
+            )
         if start[2] > 0 and start[2] <= day_in_month:
             calendar = calendar.where(
                 getattr(Calendar, f'{caltype}_day') == start[2]
-                )
+            )
         else:
             if start[2] != 0:
                 raise CalendarQueryException(
                     f' "{start[2]}" is not a valid day parameter. '
                     f'Use "0" to select all days or use anything '
                     f'between "1" and "{day_in_month}" for the month!'
-                    )
+                )
     if only_holidays:
         calendar = calendar.filter(Calendar.is_holiday == 1)
     if except_holidays:
@@ -192,7 +192,7 @@ def calender_query_builder(caltype: str, start: tuple[int, int, int],
     if len(filter_tithis) > 0:
         calendar = calendar.filter(
             Events.event_class == 'tithi', Events.event_name.in_(filter_tithis)
-            )
+        )
     if search_event.strip() != '':
         calendar = calendar.filter(Events.event_name.like(search_event))
     return calendar
@@ -205,14 +205,14 @@ def calender_result_to_dict(calendar_days, bs_as_key: bool = False):
             getattr(
                 date[0],
                 f'{"bs" if bs_as_key else "ad"}_year'
-                )
             )
+        )
         mm: str = str(
             getattr(
                 date[0],
                 f'{"bs" if bs_as_key else "ad"}_month'
-                )
             )
+        )
         if yyyy not in data:
             data[yyyy] = {}
         if mm not in data[yyyy]:
@@ -221,8 +221,8 @@ def calender_result_to_dict(calendar_days, bs_as_key: bool = False):
             getattr(
                 date[0],
                 f'{"bs" if bs_as_key else "ad"}_day'
-                )
             )
+        )
 
         data[yyyy][mm][dd] = {
             'tithi': '',
@@ -248,8 +248,19 @@ def calender_result_to_dict(calendar_days, bs_as_key: bool = False):
             else:
                 data[yyyy][mm][dd][relation.events.event_class].append(
                     relation.events.event_name
-                    )
+                )
     return data
+
+
+@app.route('/')
+def docs():
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    if os.path.exists(os.path.join(cur_dir, 'README.md')):
+        with open(os.path.join(cur_dir, 'README.md'), 'r') as f:
+            text = f.read()
+            return markdown.markdown(text)
+    else:
+        return 'MISSING DOCUMENTATION', 404
 
 
 @app.route('/v2/date/<string:caltype>/<string:date>')
@@ -261,11 +272,11 @@ def date_view(caltype: str, date: str):
             for chunk in
             calendar_var_replace(caltype=caltype, string=date).split('-')
             if chunk.strip() != ''
-            ]
+        ]
     except ValueError:
         return {
             'error': 'Invalid start date format ! Supported format: "yyyy-m-d"'
-                }, 400
+        }, 400
     try:
         calendar = calender_query_builder(
             caltype=caltype.lower(),
@@ -273,23 +284,23 @@ def date_view(caltype: str, date: str):
                 s_date[0],
                 s_date[1] if len(s_date) >= 2 else 0,
                 s_date[2] if len(s_date) >= 3 else 0
-                ),
+            ),
             only_holidays=bool(
                 int(
                     request.args.get(
                         'only_holidays', 0
-                        )
                     )
-                ),
+                )
+            ),
             except_holidays=bool(
                 int(request.args.get('except_holidays', 0))
-                ),
+            ),
             filter_tithis=request.args.get(
                 'filter_tithis', ''
-                ).split(';'),
+            ).split(';'),
             search_event=request.args.get(
                 'search', '')
-                )
+        )
         calendar_days = db.session.execute(calendar).all()
         if len(calendar_days) == 0:
             return {'error': 'No data found for date !'}, 404
@@ -299,10 +310,10 @@ def date_view(caltype: str, date: str):
                 int(
                     request.args.get(
                         'bs_as_key', 0
-                        )
                     )
                 )
             )
+        )
     except CalendarQueryException as e:
         return {'error': str(e)}, 500
 
@@ -316,22 +327,22 @@ def range(caltype: str, sdate: str, edate: str):
             for chunk in
             calendar_var_replace(caltype=caltype, string=sdate).split('-')
             if chunk.strip() != ''
-            ]
+        ]
     except ValueError:
         return {
             'error': 'Invalid start date format ! Supported format: "yyyy-m-d"'
-                }, 400
+        }, 400
     try:
         e_date = [
             int(chunk)
             for chunk in
             calendar_var_replace(caltype=caltype, string=edate).split('-')
             if chunk.strip() != ''
-            ]
+        ]
     except ValueError:
         return {
             'error': 'Invalid end date format ! Supported format: "yyyy-m-d"'
-                }, 400
+        }, 400
     try:
         calendar = calender_query_builder(
             caltype=caltype.lower(),
@@ -339,33 +350,33 @@ def range(caltype: str, sdate: str, edate: str):
                 s_date[0],
                 s_date[1] if len(s_date) >= 2 else 0,
                 s_date[2] if len(s_date) >= 3 else 0
-                ),
+            ),
             end=(
                 e_date[0],
                 e_date[1] if len(e_date) >= 2 else 0,
                 e_date[2] if len(e_date) >= 3 else 0
-                ),
+            ),
             only_holidays=bool(
                 int(
                     request.args.get(
                         'only_holidays', 0
-                        )
                     )
-                ),
+                )
+            ),
             except_holidays=bool(
                 int(
                     request.args.get(
                         'except_holidays', 0
-                        )
                     )
-                ),
+                )
+            ),
             filter_tithis=request.args.get(
                 'filter_tithis', ''
-                ).split(';'),
+            ).split(';'),
             search_event=request.args.get(
                 'search', ''
-                )
             )
+        )
         calendar_days = db.session.execute(calendar).all()
         if len(calendar_days) == 0:
             return {'error': 'No data found for date !'}, 404
@@ -375,10 +386,10 @@ def range(caltype: str, sdate: str, edate: str):
                 int(
                     request.args.get(
                         'bs_as_key', 0
-                        )
                     )
                 )
             )
+        )
     except CalendarQueryException as e:
         return {'error': str(e)}, 400
 
@@ -391,7 +402,7 @@ def today():
             'date_view',
             caltype='ad',
             date=f'{dtn.year}-{dtn.month}-{dtn.day}'
-            ) + '?' + request.query_string.decode(), 302)
+        ) + '?' + request.query_string.decode(), 302)
 
 
 @app.route('/v1/<string:opmode>')
@@ -399,10 +410,10 @@ def old_date_view(opmode: str):
     calendar_type = request.args.get('calendar', 'ad').lower()
     if calendar_type not in ['ad', 'bs']:
         return {'error': 'Invalid calendar type ! Use "ad", or "bs"'}
-    query_string = request.query_string.decode()\
-        .replace(f'calendar={request.args.get("calendar")}', '')\
-        .replace(f'date={request.args.get("date")}', '')\
-        .replace(f'start_date={request.args.get("start_date")}', '')\
+    query_string = request.query_string.decode() \
+        .replace(f'calendar={request.args.get("calendar")}', '') \
+        .replace(f'date={request.args.get("date")}', '') \
+        .replace(f'start_date={request.args.get("start_date")}', '') \
         .replace(f'end_date={request.args.get("end_date")}', '')
     if opmode == 'date':
         return redirect(
@@ -410,7 +421,7 @@ def old_date_view(opmode: str):
                 'date_view',
                 caltype=calendar_type,
                 date=request.args.get("date")
-                ) + '?' + query_string, 309)
+            ) + '?' + query_string, 309)
     elif opmode == 'range':
         return redirect(
             url_for(
@@ -418,7 +429,7 @@ def old_date_view(opmode: str):
                 caltype=calendar_type,
                 sdate=request.args.get('start_date'),
                 edate=request.args.get('end_date')
-                ) + '?' + query_string, 309)
+            ) + '?' + query_string, 309)
     else:
         return {'error': 'Invalid operation mode !'}, 400
 
@@ -433,7 +444,7 @@ if __name__ == '__main__':
             for file in artifacts_dir
             if
             file.startswith('artifact-') and file.endswith('.json')
-            ])
+        ])
         print(f'[I] Artifacts found for years:  {", ".join(str(year) for year in available_years)} ')
         for year in available_years:
             if int(os.environ.get('SKIP_DB_CREATE', 0)):
@@ -443,23 +454,23 @@ if __name__ == '__main__':
             for eng_date in cal:
                 ENG_YEAR: int = int(
                     eng_date.split('/')[0]
-                    )
+                )
                 ENG_MONTH: int = int(
                     eng_date.split('/')[1]
-                    )
+                )
                 ENG_DAY: int = int(
                     eng_date.split('/')[2]
-                    )
+                )
 
                 NEP_YEAR: int = int(
                     cal[eng_date]['nepali_date'].split('/')[0]
-                    )
+                )
                 NEP_MONTH: int = int(
                     cal[eng_date]['nepali_date'].split('/')[1]
-                    )
+                )
                 NEP_DAY: int = int(
                     cal[eng_date]['nepali_date'].split('/')[2]
-                    )
+                )
 
                 _calendar = db.session.execute(
                     db.select(Calendar).filter_by(
@@ -467,7 +478,7 @@ if __name__ == '__main__':
                         bs_sn=int(f'{NEP_YEAR}{NEP_MONTH:02d}{NEP_DAY:02d}'),
                         ad_year=ENG_YEAR, ad_month=ENG_MONTH, ad_day=ENG_DAY,
                         bs_year=NEP_YEAR, bs_month=NEP_MONTH, bs_day=NEP_DAY)
-                    ).first()
+                ).first()
                 calendar = None
                 if _calendar is None:
                     calendar = Calendar(
@@ -476,7 +487,7 @@ if __name__ == '__main__':
                         ad_year=ENG_YEAR, ad_month=ENG_MONTH, ad_day=ENG_DAY,
                         bs_year=NEP_YEAR, bs_month=NEP_MONTH, bs_day=NEP_DAY,
                         is_holiday=cal[eng_date]['is_public_holiday']
-                        )
+                    )
                     db.session.add(calendar)
                     db.session.flush()
                 else:
@@ -498,15 +509,15 @@ if __name__ == '__main__':
                     exec = db.session.execute(
                         db.select(Events).filter_by(
                             event_name=event[0], event_class=event[1]
-                            )
-                        ).first()
+                        )
+                    ).first()
                     if exec is None:
                         # Add event, it does not exist rn
                         inserted_events.append(
                             Events(
                                 event_name=event[0], event_class=event[1]
-                                )
                             )
+                        )
                         db.session.add(inserted_events[-1])
                     else:
                         inserted_p_keys.append(exec[0].id)
@@ -519,17 +530,18 @@ if __name__ == '__main__':
                     ev = db.session.execute(
                         db.select(CalendarEventRelationship).filter_by(
                             day_id=calendar.id, event_id=id
-                            )
-                        ).first()
+                        )
+                    ).first()
                     if ev is None:
                         db.session.add(
                             CalendarEventRelationship(
                                 day_id=calendar.id, event_id=id
-                                )
                             )
+                        )
                 print(
                     f'[I] Added date for year  {eng_date} '
                     f'<-> {NEP_YEAR}/{NEP_MONTH}/{NEP_DAY}'
-                    )
+                )
         db.session.commit()
-    app.run(host=os.environ.get('HOST', '0.0.0.0'), port=os.environ.get('PORT', '8080'), debug=int(os.environ.get('DEBUG', 0)))
+    app.run(host=os.environ.get('HOST', '0.0.0.0'), port=os.environ.get('PORT', '8080'),
+            debug=int(os.environ.get('DEBUG', 0)))
